cbcdb42902a9ee8bf0a02ad333d24f34abf75d07
aparkening <aaron.parkening@gmail.com>
2019-07-23 14:27:15 -0400

    Done. Create and display pirates and ships.

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+
+    # Index 
+    get '/' do
+      erb :root
+    end
+
+    # Pirate/ship form
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    # Display all information from form
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+	 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :"pirates/show"
+    end
 
   end
 end
