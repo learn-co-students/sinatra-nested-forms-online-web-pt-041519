d4a06c1dab28773cef1ae52e7eebfaa622c0ca6f
github username <github email address>
2019-07-25 00:57:19 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+# Pirates should
+# have a name, weight, and height.
+#  You will also need a class method
+#  that returns all the pirates.
+attr_accessor :name , :weight, :height
+
+@@all = []
+
+def initialize(params)
+  @name, @weight, @height = params[:name], params[:weight], params[:height]
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
 end
