f23f2e4873790fcb990ad7a8a63ec0aefca91ec6
Michael Chen <michaelchengaming@gmail.com>
2019-07-28 18:24:45 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @booty = args[:booty]
+    @type = args[:type]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
 end
