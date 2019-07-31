7869075f8ced3f040a7723552b5edf78e152ad6b
github username <github email address>
2019-07-30 21:10:21 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def initialize(params)
+    # binding.pry
+    self.name, self.type, self.booty = params[:name], params[:type], params[:booty]
+    self.class.all << self
+  end
+
 end
